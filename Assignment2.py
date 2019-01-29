7190a271cbe9066735166c40becc306d07b51b01
mephi009 <roysumit949@gmail.com>
2019-01-29 23:18:17 +0530

    assignment2

---
+++ Assignment2.py
@@ -0,0 +1,18 @@
+#Write a program to find the biggest of 3 numbers (Use If Condition)
+
+#taking three inputs from user
+print("enter three numbers")
+a = input()
+b = input()
+c = input()
+
+#using if to check if a is biggest
+if(a>b & a>c):
+	print("the biggest number among three is ::"+ str(a))
+
+#if a is not, then checking whether b is greater than c
+elif (b>c):
+	print("the biggest number among three is ::"+ str(b))
+#then c will be biggest
+else:
+	print("the biggest number among three is ::"+ str(c))
