046f626729b2593104dd1b5ba816599da08b1dda
mephi009 <roysumit949@gmail.com>
2019-02-02 09:56:19 +0530

    Assignment10

---
+++ Assignment9.py
@@ -0,0 +1,20 @@
+#Write program to Add, Subtract, Multiply, Divide 2 Complex numbers.
+
+complex1 = eval(input("enter 1st complex number"))
+complex2  = eval(input("enter 2nd complex number"))
+
+#addition
+add = complex1 + complex2
+print("addition of two complex number", add)
+
+#subtraction
+sub = complex1 - complex2
+print("subtraction of two complex number", sub)
+
+#multiplication
+mul = complex1 * complex2
+print("multiplication of two complex number", mul)
+
+#division
+div = complex1 / complex2
+print("division of two complex number", div)
